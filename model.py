a73db336ce17c197670dffdfa76b992976e7cbfb
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2025-04-20 23:09:03 -0400

    Defining the initial model structure

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+class PubMedLSTMClassifier(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, num_layers=1, drop_prob=0.5):
+        super(PubMedLSTMClassifier, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, batch_first=True, dropout=drop_prob)
+        self.dropout = nn.Dropout(drop_prob)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        lstm_out, _ = self.lstm(embedded)
+        out = self.dropout(lstm_out[:, -1, :])
+        return self.softmax(self.fc(out))
