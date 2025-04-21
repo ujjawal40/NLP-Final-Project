8e458a5622e802c4268a3b703c84e552f6f31c66
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2025-04-20 22:29:43 -0400

    Initial Start to the model, started by doing basic imports

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+import os
+import random
