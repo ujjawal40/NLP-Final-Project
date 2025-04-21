914b1e871aabb171a24313848e4d5cab50f19ac0
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2025-04-20 22:28:59 -0400

    Initial Start to the model, started by doing basic imports

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import re
+from collections import Counter
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
